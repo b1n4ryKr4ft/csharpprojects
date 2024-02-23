b70100e9f92a92168fa16d2cf2c967906aeaf49a
b1n4ryKr4ft <m4bu7h0@gmail.com>
2024-02-23 04:59:03 +0000

    Format Complete

--- FormatAlphaNumericData/Program.cs
+++ FormatAlphaNumericData/Program.cs
@@ -20,7 +20,6 @@ string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} pri
 
 yourDiscount += $"A discount of {((price - salePrice)/price):P2}!".Replace(",","."); //inserted
 Console.WriteLine(yourDiscount);
-*/
 
 int invoiceNumber = 1201;
 decimal productShares = 25.4568m;
@@ -33,3 +32,59 @@ Console.WriteLine($"   Shares: {productShares:N3} Product".Replace(",", "."));
 Console.WriteLine($"     Sub Total: {subtotal:C}".Replace(",", "."));
 Console.WriteLine($"           Tax: {taxPercentage:P2}".Replace(",", "."));
 Console.WriteLine($"     Total Billed: {total:C}".Replace(",", "."));
+
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
+ Console.WriteLine(input.PadRight(12));
+ 
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
