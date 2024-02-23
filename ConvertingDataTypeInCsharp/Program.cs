0089cf4a92d079185275be55eb81a24436b0fbf8
b1n4ryKr4ft <m4bu7h0@gmail.com>
2024-02-23 00:34:58 +0000

    finished

--- ConvertingDataTypeInCsharp/Program.cs
+++ ConvertingDataTypeInCsharp/Program.cs
@@ -44,10 +44,9 @@ Console.WriteLine(value);
 
 int value2 = Convert.ToInt32(1.5m); // converting rounds up
 Console.WriteLine(value2);
-*/
 
 string value = "bad";
-int result;
+int result = 0;
 if (int.TryParse(value, out result))
 {
    Console.WriteLine($"Measurement: {result}");
@@ -58,3 +57,39 @@ else
 }
 if (result > 0)
    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+*/
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {Convert.ToInt32(value1/value2)}");
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {(decimal)(value2/(decimal)value3)}");
+
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {(float)(value3/value1)}");
