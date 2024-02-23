77741bcbf0408293dc0692064aac830e744107ad
b1n4ryKr4ft <m4bu7h0@gmail.com>
2024-02-23 01:57:17 +0000

    no using necessary

--- OperationsOnArrayUsingHelpers/Program.cs
+++ OperationsOnArrayUsingHelpers/Program.cs
@@ -90,8 +90,6 @@ string result = String.Join(" ", newMessage);
 Console.WriteLine(result);
 */
 
-using System;
-
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
 var orderIDs = orderStream.Split(',');
