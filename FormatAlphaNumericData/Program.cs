6cd40fe14dd10f3fd35e79d606b7a0d7238d30b4
b1n4ryKr4ft <m4bu7h0@gmail.com>
2024-02-23 03:44:53 +0000

    Interpolation

--- FormatAlphaNumericData/Program.cs
+++ FormatAlphaNumericData/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+CultureInfo.CurrentCulture = new CultureInfo("en-ZA");
+
+/*decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N2} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+*/
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
